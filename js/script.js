cd9dd09cf66bf71a076bbcdcc913874ccc96ba80
Emil Konggaard Schou <emil587a@edu.campusvejle.dk>
2018-12-03 12:06:02 +0100

    added FizzBuzz

--- js/script.js
+++ js/script.js
@@ -24,20 +24,17 @@ $(document).ready(function(){
 		}
 	});
 	
-	function fizzbuzz(){
+	
+    function fizzbuzz(){
 		for(var i = 1; i <= 100; i++){
-			if(i%3 === 0){
-				if(i%5 === 0){
-					console.log("FizzBuzz");
-				}else{
-					console.log("Fizz");
-				}
+			if(i%3 && i%5 === 0){
+				console.log("FizzBuzz");
+			}else if(i%3 === 0){
+				console.log("Fizz");
+			}else if(i%5 === 0){
+				console.log("Buzz");
 			}else{
-				if(i%5 === 0){
-					console.log("Buzz");
-				}else{
-					console.log(i);
-				}
+				console.log(i);
 			}
 		}
 	}
