f7de007ba8558788eb25b4546c87ba0655ff8c39
Emil Konggaard Schou <emil587a@edu.campusvejle.dk>
2018-12-03 13:39:15 +0100

    Added FizzBuzz

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
 	
     function fizzbuzz(){
 		for(var i = 1; i <= 100; i++){
-			if(i%3 && i%5 === 0){
+			if(i%3 === 0 && i%5 === 0){
 				console.log("FizzBuzz");
 			}else if(i%3 === 0){
 				console.log("Fizz");
